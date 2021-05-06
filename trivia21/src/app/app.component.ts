ff17f0bbaff3e6b7705ab174b021d646f95c8f6f
Jessica Kincaid <jessicakincaid@jessicasmacbook.home>
2021-05-06 09:51:57 -0500

    deleted routertags

--- trivia21/src/app/app.component.ts
+++ trivia21/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Drinks are on the house!';
+  title = 'The class app component contains business logic!';
 }
